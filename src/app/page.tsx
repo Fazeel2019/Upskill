a3425da768d91985d1ebe2cc9bb15fb652153359
Fazeel Butt <fazeelbutt.fb@gmail.com>
2025-10-09 19:25:37 +0000

    on mobile and tab  view make it bigger so  it can cover the empty spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,9 +139,9 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                 <div className="relative h-80 md:h-[500px] w-full order-1 md:order-2">
+                 <div className="relative h-96 md:h-[500px] w-full order-1 md:order-2">
                     <div className="relative z-0 h-full w-full rounded-2xl overflow-hidden shadow-2xl">
-                      <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzNmOG80eXR6NXRmanFxZXIwaG10NzR1aGc1MnR5MHJ1czB1cnFidSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gr0FZsazDgg3NzkHc8/giphy.gif" alt="Diverse group of healthcare and STEM professionals with UI elements" fill style={{objectFit:"contain"}} sizes="(max-width: 768px) 100vw, 50vw" data-ai-hint="diverse professionals community" unoptimized className="rounded-2xl"/>
+                      <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzNmOG80eXR6NXRmanFxZXIwaG10NzR1aGc1MnR5MHJ1czB1cnFidSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gr0FZsazDgg3NzkHc8/giphy.gif" alt="Diverse group of healthcare and STEM professionals with UI elements" fill style={{objectFit:"cover"}} sizes="(max-width: 768px) 100vw, 50vw" data-ai-hint="diverse professionals community" unoptimized className="rounded-2xl"/>
                     </div>
                     <AnimatedTag className="top-0 left-[-20px] hidden md:block" animation={floatingAnimation(0, 6)}>
                       <div className="flex items-center gap-3">
@@ -444,6 +444,7 @@ export default function Home() {
     
 
     
+
 
 
 
